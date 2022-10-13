3929f082cebc3fe482cf753a3a9810f03efb336e
Hunter Monroe <huntis@outlook.com>
2022-10-13 17:08:48 -0600

    Feature/Navbar

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './Navbar.css'
+
+function Navbar(){
+    return (
+    <nav className="Navbar"> 
+        <p>Hello Dani</p>
+    </nav>
+    )
+}
+
+export default Navbar
