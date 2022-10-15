6c5fc199e018829530f75c6b35f9976bb1f159c8
Hunter Monroe <huntis@outlook.com>
2022-10-15 12:21:35 -0600

    Refactor movie info to take props data

--- src/components/MovieInfo/MovieInfo.js
+++ src/components/MovieInfo/MovieInfo.js
@@ -1,47 +1,36 @@
 import React from "react";
 import './MovieInfo.css'
 
-const MovieInfo = () => {
-    return (
+const MovieInfo = (movieDetails) => {
+    const move = movieDetails.movieDetails.map(detail => detail.movie)
+    console.log(move[0].title, 'this')
+    return (movieDetails.popup) ? (
         <div className="movie-detail-container">
             <section className="movie-detail-section">
                 <div className="backdrop-img-container">
-                    <img src="https://image.tmdb.org/t/p/original//oazPqs1z78LcIOFslbKtJLGlueo.jpg"></img>
+                    <img src= {move[0].backdrop_path}></img>
                 </div>
                 <div className="movie-info-container">
                     <div className="movie-poster-container">
-                        <img src="https://image.tmdb.org/t/p/original//7G2VvG1lU8q758uOqU6z2Ds0qpA.jpg"></img>
+                        <img src={move[0].poster_path}></img>
                     </div>
                     <div className="movie-description-container">
-                        <h2 className="movie-title">Fake Movie Title</h2>
-                        <p className="release-date">2019-12-04</p>
-                        <p className="overview">Some overview that is full of buzzwords to attempt to entice you to watch this movie! Explosions! Drama! True love! Robots! A cute dog!</p>
-                        <p className="avg-rating">6</p>
-                        <p className="genre">Drama</p>
-                        <p className="budget">63000000</p>
-                        <p className="revenue">100853753</p>
-                        <p className="runtime">139</p>
-                        <p className="tagline">`It's a movie!`</p>
+                        <h2 className="movie-title"> {move[0].title}</h2>
+                        <p className="release-date">{move[0].release_date}</p>
+                        <p className="overview">{move[0].overview}</p>
+                        <p className="avg-rating">{move[0].average_rating}</p>
+                        <p className="genre">{move[0].genres}</p>
+                        <p className="budget">{move[0].budget}</p>
+                        <p className="revenue">{move[0].revenue}</p>
+                        <p className="runtime">{move[0].runtime}</p>
+                        <p className="tagline">{move[0].tagline}</p>
                     </div>
                 </div>
             </section>
         </div>
-    )
+    ) : " "; 
 
 }
 
 export default MovieInfo;
 
-// {"movie": {
-//     id: 1, title: "Fake Movie Title", 
-//     poster_path: "https://image.tmdb.org/t/p/original//7G2VvG1lU8q758uOqU6z2Ds0qpA.jpg", 
-//     backdrop_path: "https://image.tmdb.org/t/p/original//oazPqs1z78LcIOFslbKtJLGlueo.jpg", 
-//     release_date: "2019-12-04", 
-//     overview: "Some overview that is full of buzzwords to attempt to entice you to watch this movie! Explosions! Drama! True love! Robots! A cute dog!", 
-//     average_rating: 6, 
-//     genres: ["Drama"], 
-//     budget:63000000, 
-//     revenue:100853753, 
-//     runtime:139, 
-//     tagline: "It's a movie!" 
-// }}
