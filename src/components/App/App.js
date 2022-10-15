a69b4a1b937c62778ca31b36f57b69560cbd2b31
Hunter Monroe <huntis@outlook.com>
2022-10-15 14:52:06 -0600

    Changed hard coded data

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -12,20 +12,20 @@ class App extends Component{
     this.state = {
       movies:[], 
       individualMovie: [
-        {"movie": {
-        id: 1, 
-        title: "Fake Movie Title", 
-        poster_path: "https://image.tmdb.org/t/p/original//7G2VvG1lU8q758uOqU6z2Ds0qpA.jpg", 
-        backdrop_path: "https://image.tmdb.org/t/p/original//oazPqs1z78LcIOFslbKtJLGlueo.jpg", 
-        release_date: "2019-12-04", 
-        overview: "Some overview that is full of buzzwords to attempt to entice you to watch this movie! Explosions! Drama! True love! Robots! A cute dog!", 
-        average_rating: 6, 
-        genres: ["Drama"], 
-        budget:63000000, 
-        revenue:100853753, 
-        runtime:139, 
-        tagline: "It's a movie!" 
-        }}
+        // {"movie": {
+        // id: 1, 
+        // title: "Fake Movie Title", 
+        // poster_path: "https://image.tmdb.org/t/p/original//7G2VvG1lU8q758uOqU6z2Ds0qpA.jpg", 
+        // backdrop_path: "https://image.tmdb.org/t/p/original//oazPqs1z78LcIOFslbKtJLGlueo.jpg", 
+        // release_date: "2019-12-04", 
+        // overview: "Some overview that is full of buzzwords to attempt to entice you to watch this movie! Explosions! Drama! True love! Robots! A cute dog!", 
+        // average_rating: 6, 
+        // genres: ["Drama"], 
+        // budget:63000000, 
+        // revenue:100853753, 
+        // runtime:139, 
+        // tagline: "It's a movie!" 
+        // }}
       ]
     }
   }
@@ -40,14 +40,14 @@ getIndividualMovie(id){
   console.log(id)
   fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}`)
   .then(response => response.json())
-  // .then(data => this.setState({individualMovie:data.movie}))
+  .then(data => this.setState({individualMovie:data}))
   .then(data => console.log(data.movie))
   .catch(error => console.log(error))
 }
 
 
 render() {
-  // console.log(this.state.movies)
+  console.log(this.state.individualMovie)
   return (
     <div className="App">
       <header> 
@@ -56,7 +56,7 @@ render() {
       </header>
       <main >
         <Movies movieData = {this.state.movies} getDetails ={this.getIndividualMovie} />
-        <MovieInfo movieDetails = {this.state.individualMovie} popup = {true}/>
+        <MovieInfo movieDetails = {this.state.individualMovie} popup = {false}/>
       </main>
     </div>
   );
