4e522ad93ae96e6113e2c2247922cff86dbaf624
Hunter Monroe <huntis@outlook.com>
2022-10-16 13:56:23 -0600

    Feat/ created api calls

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,32 +4,18 @@ import Navbar from '.././Navbar/Navbar';
 // import movieData from '../../MockMovieData.js';
 import Movies from '.././Movies/Movies'
 import MovieInfo from '.././MovieInfo/MovieInfo'
-import {useState} from "react"
 
 
-class App extends Component{
+
+class App extends Component {
   constructor(){
     super(); 
     this.state = {
       movies:[], 
       individualMovie:[]
-        // {"movie": {
-        // id: 1, 
-        // title: "Fake Movie Title", 
-        // poster_path: "https://image.tmdb.org/t/p/original//7G2VvG1lU8q758uOqU6z2Ds0qpA.jpg", 
-        // backdrop_path: "https://image.tmdb.org/t/p/original//oazPqs1z78LcIOFslbKtJLGlueo.jpg", 
-        // release_date: "2019-12-04", 
-        // overview: "Some overview that is full of buzzwords to attempt to entice you to watch this movie! Explosions! Drama! True love! Robots! A cute dog!", 
-        // average_rating: 6, 
-        // genres: ["Drama"], 
-        // budget:63000000, 
-        // revenue:100853753, 
-        // runtime:139, 
-        // tagline: "It's a movie!" 
-        // }}
     }
     this.isClicked = false;
-    // let [buttonPopup, setButtonPopup] = useState(false);
+    this.homepageView = true;
   }
 
 componentDidMount(){
@@ -39,29 +25,41 @@ componentDidMount(){
 }
 
 getIndividualMovie = (id) => {
-  // let [buttonPopup, setButtonPopup] = useState(false);
-  this.isClicked = true;
-  console.log(id)
+  // this.showDetails()
   fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}`)
+  // .then(response => console.log(response))
   .then(response => response.json())
-  .then(data => this.setState({individualMovie:data.movie}))
-  .then(data => console.log(data.movie))
-  .catch(error => console.log(error))
+  .then(data => this.setState({individualMovie: data.movie}))
+  // .then(data => console.log(data.movie))
+  .catch(error => console.log('there is an error', error))
   // setButtonPopup(true);
+  this.showDetails()
+}
+
+showDetails = () => {
+  this.isClicked = true
+  this.homepageView = false
+  console.log('im working')
+}
+hideDetails = () => {
+  this.setState({individualMovie: []})
+  this.isClicked = false
+  this.homepageView = true
+  console.log('hello')
 }
 
 render() {
-  console.log("over here", this.state.individualMovie)
   return (
     <div className="App">
       <header> 
         <h1>Rancid Tomatillos</h1>
-        <Navbar />
+        <Navbar hideDetails = {this.hideDetails}/>
       </header>
       <main >
         <Movies 
           movieData = {this.state.movies} 
           getDetails ={this.getIndividualMovie}
+          homepageView = {this.homepageView}
           // setTriggerPopup = {setButtonPopup} 
           />
       </main>
