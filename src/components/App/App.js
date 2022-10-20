a8eb9fedd296ad9258072740dd08633d7b8d108a
Dani Lopez-Cruz <roberto.lc2493@gmail.com>
2022-10-19 20:37:55 -0600

    Micro: Add minor CSS refactor

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -17,69 +17,66 @@ class App extends Component {
     this.homepageView = true;
   }
 
-componentDidMount(){
-  fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies')
-    .then(response => {
-      if(!response.ok) {
-        throw `${response.status} ${response.statusText}`;
-      } else {
-        return response.json();
-      }
-    })
-    .then(data => this.setState({movies:data.movies}))
-    .catch(err => {
-      this.setState({error : err});
-    })
+  componentDidMount(){
+    fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies')
+      .then(response => {
+        if(!response.ok) {
+          throw `${response.status} ${response.statusText}`;
+        } else {
+          return response.json();
+        }
+      })
+      .then(data => this.setState({movies:data.movies}))
+      .catch(err => {
+        this.setState({error : err});
+      })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <h1>Rancid Tomatillos</h1>
+          <nav className="Navbar"  >
+            <NavLink to='/' >
+                Home 
+            </NavLink>
+          </nav>
+        </header>
+
+        {this.state.error ? 
+          <ErrorHandle errorStatus = {this.state.error}/>
+          : 
+          <Switch>
+            <Route exact path='/'>
+              <Movies movieData ={this.state.movies}/>
+            </Route>
+          
+            <Route path='/:id' render={({match}) => {
+              return <MovieInfo id ={match.params.id} />
+            }} />
+          </Switch>
+        }
+
+
+        {/* CONDITIONAL RENDERING - MOVIES RENDERING
+        {this.state.movies &&  
+        <Movies 
+          movieData = {this.state.movies} 
+          getDetails ={this.getIndividualMovie}
+          homepageView = {this.homepageView}
+          // setTriggerPopup = {setButtonPopup} 
+          />
+        } */}
+
+        {/* CONDITIONAL RENDERING - INDIVIDUAL MOVIE'S DETAILS
+        {this.state.individualMovie &&
+        <MovieInfo 
+          movieDetails = {this.state.individualMovie}  />
+        } */}
+      </div>
+    );
+  }
 }
-
-render() {
-  return (
-    <div className="App">
-      <header>
-        <h1>Rancid Tomatillos</h1>
-        <nav className="Navbar"  >
-          <NavLink to='/' >
-              Home 
-          </NavLink>
-        </nav>
-      </header>
-
-      {this.state.error ? 
-        <ErrorHandle errorStatus = {this.state.error}/>
-        : 
-        <Switch>
-          <Route exact path='/'>
-            <Movies movieData ={this.state.movies}/>
-          </Route>
-        
-          <Route path='/:id' render={({match}) => {
-            return <MovieInfo id ={match.params.id} />
-          }} />
-        </Switch>
-      }
-
-
-      {/* CONDITIONAL RENDERING - MOVIES RENDERING
-      {this.state.movies &&  
-      <Movies 
-        movieData = {this.state.movies} 
-        getDetails ={this.getIndividualMovie}
-        homepageView = {this.homepageView}
-        // setTriggerPopup = {setButtonPopup} 
-        />
-      } */}
-
-      {/* CONDITIONAL RENDERING - INDIVIDUAL MOVIE'S DETAILS
-      {this.state.individualMovie &&
-      <MovieInfo 
-        movieDetails = {this.state.individualMovie}  />
-      } */}
-    </div>
-  );
-}
-
-}
-
-
 
 export default App;
