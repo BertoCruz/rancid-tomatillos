c1f305ee8cdacb979a3a47b56b3eef138bb8e1c3
Dani Lopez-Cruz <roberto.lc2493@gmail.com>
2022-10-21 17:12:11 -0600

    Added an Api-Calls.js file to hold the fetch calls

---
+++ src/api-calls.js
@@ -0,0 +1,23 @@
+const fetchMoviesData = () => {
+    return fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies`)
+        .then((response) => {
+            if(!response.ok) {
+                throw `${response.status} ${response.statusText}`;
+              } else {
+                return response.json();
+              }            
+        })
+}
+
+const fetchIndividualMovie = (endpoint) => {
+    return fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${endpoint}`)
+        .then((response) => {
+            if(!response.ok) {
+                throw `${response.status} ${response.statusText}`;
+              } else {
+                return response.json();
+              }
+        })
+}
+
+export { fetchMoviesData, fetchIndividualMovie };
